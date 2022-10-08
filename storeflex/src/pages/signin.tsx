122343c80080b1172871b3aa5027c3ec10a8a01d
Samaruzzaman <113083938+Samaruzzaman@users.noreply.github.com>
2022-10-08 15:00:50 +0530

    Google Login

--- storeflex/src/pages/signin.tsx
+++ storeflex/src/pages/signin.tsx
@@ -1,163 +1,160 @@
 import React, { useState, useEffect } from 'react';
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import Container from '@mui/material/Container';
-import { TextField, Typography, Avatar, Box, Button, Link, Grid} from '@mui/material';
+import { TextField, Typography, Avatar, Box, Button, Link, Grid } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import CheckBoxR from '../components/atoms/checkbox/CheckBoxR';
 import { validateEmail, validatePassword } from '../utils/CommonUtils';
 import AppleIcon from '@mui/icons-material/Apple';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
-import SocialButton from '../components/atoms/social/SocialButton'
+import GoogleLogin from 'react-google-login';
+import { gapi } from "gapi-script";
 
 const SignIn = () => {
 
-const [values, setValues] = useState({
-  email : "",
-  password : "",
-});
-
-const [errors, setErrors] = useState({
-  email:"",
-  password:"",
-
-});
-
-const setNodeRef = (provider: any, node: any) => {
-  // if (node) {
-  //   this.nodes[ provider ] = node
-  // }
-}
-
-const onLoginSuccess = (user: any) => {
-  console.log("Login Success====",user)
-}
-
-const onLoginFailure = (err: any) => {
-  console.error("Login Failure",err)
-}
-
-const onLogoutSuccess = () => {
-}
-
-const onLogoutFailure = (err: any) => {
-  console.error(err)
-}
-
-const handleChange = (event: any) =>{
-  setValues({
-    ...values,
-    [event.target.name]: event.target.value,
+  gapi.load("client:auth2", () => {
+    gapi.client.init({
+      clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      plugin_name: "storeflex",
+      scope: 'email',
+    });
   });
-};
 
-const validation = (values:any) => {
-  console.log("Values==", values)
-  let errors = {
+  const [values, setValues] = useState({
     email: "",
-    password: ""
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+
+  });
+
+  const handleChange = (event: any) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
   };
 
-  //Email validation check
-  if (!values.email){
-    errors.email = "Email is required"
-  }
-  else if (!validateEmail(values.email)) {
-    errors.email = "Enter a valid mail"
-  }
+  const validation = (values: any) => {
+    console.log("Values==", values)
+    let errors = {
+      email: "",
+      password: ""
+    };
 
-  //password validation check
-  if(!values.password){
-    errors.password = "Password is required."
+    //Email validation check
+    if (!values.email) {
+      errors.email = "Email is required"
+    }
+    else if (!validateEmail(values.email)) {
+      errors.email = "Enter a valid mail"
+    }
+
+    //password validation check
+    if (!values.password) {
+      errors.password = "Password is required."
+    }
+    else if (!validatePassword(values.password)) {
+      errors.password = "Enter valid password"
+    }
+    return errors;
   }
-  else if (!validatePassword(values.password)){
-    errors.password = "Enter valid password"
-  }
-  return errors;
-}
 
   const navigate = useNavigate();
   const submitSignIn = () => {
     setErrors(validation(values));
-      navigate('/dashboard');
+    navigate('/dashboard');
   }
   const handleSubmit = (event: any) => {
     event.preventDefault();
     setErrors(validation(values));
   };
-  
-    return (
-      <>
-      <Container component="main" maxWidth="xs" className='c-box-shadow p-no'>
-      <div className='blue-gradient'> 
-      <div className='font-white p-md'><b>
-      <Typography component="h1" variant="h4">
-            Sign in
-          </Typography>
-        </b></div>
-      </div>
-      <div className='p-md'>
-        <Box sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}> </Grid>
-              <Grid item xs={12}>
-          <TextField
-              // margin="normal"
-              // required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              value={values.email}
-              onChange={handleChange}
-              autoFocus
-            />
-            {errors.email && <p className="text-red">{errors.email}</p>}
-            </Grid>
-            <br></br>
-            <Grid item xs={12}>
-            <TextField
-              // margin="normal"
-              // required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={values.password}
-              onChange={handleChange}
-            />
-            {errors.password && <p className="text-red">{errors.password}</p>}
-            </Grid>
-            <div>
-              <div className='p-top-sm'>
 
-                <CheckBoxR />
-                <span className='font-gray font-12px'>This is a public or shared device.</span>
-              </div>
-              <div className='p-top-md'>
-                <div className='font-12px'>
+  const onGoogleLoginSuccess = (user: any) => {
+    console.log("Login Success====", user);
+    navigate('/dashboard');
+  };
+
+  const onGoogleLoginFailure = (err: any) => {
+    console.error("Login Failure", err)
+  };
+
+  return (
+    <>
+      <Container component="main" maxWidth="xs" className='c-box-shadow p-no'>
+        <div className='blue-gradient'>
+          <div className='font-white p-md'><b>
+            <Typography component="h1" variant="h4">
+              Sign in
+            </Typography>
+          </b></div>
+        </div>
+        <div className='p-md'>
+          <Box sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}> </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  // margin="normal"
+                  // required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  autoFocus
+                />
+                {errors.email && <p className="text-red">{errors.email}</p>}
+              </Grid>
+              <br></br>
+              <Grid item xs={12}>
+                <TextField
+                  // margin="normal"
+                  // required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={values.password}
+                  onChange={handleChange}
+                />
+                {errors.password && <p className="text-red">{errors.password}</p>}
+              </Grid>
+              <div>
+                <div className='p-top-sm'>
+
+                  <CheckBoxR />
+                  <span className='font-gray font-12px'>This is a public or shared device.</span>
+                </div>
+                <div className='p-top-md'>
+                  <div className='font-12px'>
                     <span className='font-gray'>By signing in. I agree to the Expedia </span>
                     <span>
                       <Link href="#" underline="none">{'Terms and Condition.'}</Link>
                     </span>
-                </div>
-                <div className='font-12px'>
-                  <Link href="#" underline="none">{'Privacy Statement'}</Link>
-                  <span className='font-gray'>{' and '}</span>
-                  <Link href="#" underline="none">{'Expedia Rewards Terms and Conditions.'}</Link>
+                  </div>
+                  <div className='font-12px'>
+                    <Link href="#" underline="none">{'Privacy Statement'}</Link>
+                    <span className='font-gray'>{' and '}</span>
+                    <Link href="#" underline="none">{'Expedia Rewards Terms and Conditions.'}</Link>
+                  </div>
                 </div>
               </div>
-            </div>
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} 
-              onClick= {() =>{ submitSignIn()}}>
-              Sign In
-            </Button>
+              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}
+                onClick={() => { submitSignIn() }}>
+                Sign In
+              </Button>
             </Box>
             <div className='font-12px p-top-md'>
               <Link href="#" underline="none">{'Forgot password'}</Link>
@@ -168,45 +165,22 @@ const validation = (values:any) => {
             </div>
             <div className='font-12px p-top-lg'>
               <span className='font-gray'> Or continue with </span>
-            </div>
-            <div className='font-12px p-top-md'> <SocialButton
-                provider='google'
-                appId='1047014629108-llpv3o8hr89bjkjiq4jl17htd34mafhg.apps.googleusercontent.com'
-                onLoginSuccess={onLoginSuccess}
-                onLoginFailure={onLoginFailure}
-                onLogoutSuccess={onLogoutSuccess}
-                onLogoutFailure={onLogoutFailure}
-                getInstance={setNodeRef.bind(this, 'google')}
-                key={'google'}
-                scope={'https://www.googleapis.com/auth/user.gender.read'}
-
-              >
-                Login with Google
-              </SocialButton>
-              <SocialButton
-                provider='facebook'
-                appId='657319568198782'
-                onLoginSuccess={onLoginSuccess}
-                onLoginFailure={onLoginFailure}
-                onLogoutSuccess={onLogoutSuccess}
-                getInstance={setNodeRef.bind(this, 'facebook')}
-                key={'facebook'}
-                onInternetFailure={() => { return true }}
-              >
-                Login with Facebook
-              </SocialButton>
-
+            </div><div className='font-12px p-top-md'>
               <Grid container spacing={2} columns={{ xs: 4, sm: 4, md: 4 }}>
-                  <Grid item ><AppleIcon /></Grid>
-                  <Grid item ><FacebookIcon color="primary"/></Grid>
-                  <Grid item ><GoogleIcon color="success"/></Grid>
+                <GoogleLogin
+                  clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ""}
+                  buttonText=" Google"
+                  onSuccess={onGoogleLoginSuccess}
+                  onFailure={onGoogleLoginFailure}
+                  cookiePolicy={'single_host_origin'}
+                />
               </Grid>
             </div>
-        </Box>
+          </Box>
         </div>
       </Container>
-      </>
-    );
-  };
-  
-  export default SignIn;
+    </>
+  );
+};
+
+export default SignIn;
--- storeflex/src/utils/CommonUtils.ts
+++ storeflex/src/utils/CommonUtils.ts
@@ -4,7 +4,7 @@ export const regex_specialChar = /^[A-Za-z ]+$/;
 export const regex_pass = /4^((?!.*[\s])(?=.*[A-Z])(?=.*\d).{8,15})/;
 export const regex_url = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 export const regex_phone = /^[6789]\d{9}$/;
-export const regex_gstid = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/;
+export const regex_gstid = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/; // Add comment
 
 export const validateCharacterLength = (text: string, minLen:number, maxLen:number) => {
     const isValid = (text.length < minLen || text.length > maxLen)?false:true;
