308a58ecfb097e694d0f4c8e48141f3c68d07c50
Samaruzzaman <113083938+Samaruzzaman@users.noreply.github.com>
2022-10-14 02:47:14 +0530

    New Template Integration

---
+++ storeflex/src/components/miscellaneous/brand.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Brand = () => {
+    return (
+      <>
+<div className="brand-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-5 col-12">
+              <div className="section-title align-left">
+                <span>Our Awesome Clients</span>
+                <h2>We built Best solutions for your company</h2>
+                <p>
+                  There are many variations of passages of Lorem Ipsum available,
+                  but the majority have suffered alteration in some form.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-7 col-12">
+              <div className="brand-logo-wrapper">
+                <ul className="brand-list liststyle d-flex flex-wrap justify-content-center">
+                  <li>
+                    <a href="javascript:void(0)">
+                      <img src="assets/images/client-logo/graygrids.svg" alt="Brand Logo Images" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)">
+                      <img src="assets/images/client-logo/uideck.svg" alt="Brand Logo Images" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)">
+                      <img src="assets/images/client-logo/ayroui.svg" alt="Brand Logo Images" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)">
+                      <img src="assets/images/client-logo/ecomhtml.svg" alt="Brand Logo Images" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)">
+                      <img src="assets/images/client-logo/lineicons.svg" alt="Brand Logo Images" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)">
+                      <img src="assets/images/client-logo/tailwindtemplates.svg" alt="Brand Logo Images" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
+    );
+};
+export default Brand;
