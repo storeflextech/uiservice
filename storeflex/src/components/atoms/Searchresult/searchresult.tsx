dea7b96bbec298b7089a169e31a6405cc65f63cd
Rupon Sarkar <rupon.skaplink@gmail.com>
2022-10-14 09:20:53 +0530

    filter component worked

--- storeflex/src/components/atoms/Searchresult/searchresult.tsx
+++ storeflex/src/components/atoms/Searchresult/searchresult.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom"
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
@@ -22,102 +23,99 @@ const Item = styled(Paper)(({ theme }) => ({
 
 
 export default function Searchresult() {
+  
+const [office, setOffice] = useState<Array<any>>([]);
+const [filter, setFilter] = useState('');
+const navigate = useNavigate();
 
-  const [office, setOffice] = useState<Array<any>>([]);
-  const [filter, setFilter] = useState('asc');
-
-  useEffect(() => {
+  useEffect(()=>{
     const data = ([
       {
-        id: 1,
-        name: "Office 1",
+        id:1,
+        business: "AAA Self Storage",
+        name: "Abc Office",
+        address: "Six mile, Guwahati-01",
         rate: 58,
+        
       },
       {
-        id: 2,
-        name: "Office 2",
+        id:2,
+        business: "BBB Self Storage",
+        name: "Cde Office",
+        address: "Dispur, Guwahati-02",
         rate: 56,
       },
       {
-        id: 3,
-        name: "Office 3",
+        id:3,
+        business: "CCC Self Storage",
+        name: "Efg office",
+        address: "Ganesguri, Guwahati-01",
         rate: 59,
       },
       {
-        id: 4,
-        name: "Office 4",
+        id:4,
+        business: "DDD Self Storage",
+        name: "Hij Office",
+        address: "Pan bazar, Guwahati-01",
         rate: 54,
       },
       {
-        id: 5,
-        name: "Office 5",
+        id:5,
+        business: "EEE Self Storage",
+        name: "Klm office",
+        address: "Paltan Bazar, Guwahati-01",
         rate: 64,
       }
     ])
 
-    // setOffice(data);
-
-    // let sortedData = data.sort((a, b) =>  b.rate - a.rate
-    // );
-
-
     setOffice(data);
-
+    console.log(office)
 
   }, [])
 
 
-  function sortAscending() {
-    var sortedData = office.sort((a, b) => {
-      return a.rate - b.rate
-    });
 
-    setFilter('dsc');
-    setOffice(sortedData);
+ const addToCart=(e:any,selectedItem:any) => {
+   console.log("Selected Item===",selectedItem);
+   navigate('/cart');
+ }
 
-  }
 
-  function sortDecending() {
-    var sortedData = office.sort((a, b) => {
-      return b.rate - a.rate
-    });
 
-    setFilter('asc');
-    setOffice(sortedData);
-  }
-
-  console.log("status");
+ const handleFilte =(data:any, filter:any)=>{
+  
+  setFilter(filter);
+  setOffice(data);
+  
+ }
 
   return (
-
     <Box sx={{ width: '100%' }}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-        <Container maxWidth="xl">
+        <Container maxWidth="xl">        
           <Box sx={{
             display: 'flex',
             flexDirection: 'row',
             borderRadius: 1,
           }}>
 
-            <Grid item md={2}>
-              <Filter />
-            </Grid>
-
-            <Grid item md={10} sx={{ pl: 1 }}>
+            {<Filter officeData={office} handleFilte={handleFilte}/>}
+            
+            <Grid item xs={10} sx={{ pl: 1 }}>
 
               {office.map((data) => (
 
-                <Item key={data.id} sx={{ mb: 1 }}>
+                <Item key={data.id} sx={{mb:1}}>
                   <Grid item xs={12} sx={{ p: 2 }}>
                     <div className='text-left'>
-                      <div className='header'> <BeenhereIcon /> AAA Self Storage </div>
+                      <div className='header'> <BeenhereIcon /> {data.business} </div>
                     </div>
                     <Grid container spacing={2}>
                       <Grid item sm={3}>
                         <div className='card'>
                           <div className='text-left'>
                             <div className='sub-header'> {data.name} </div>
-                            <div> Six mile, Guwahati, 781005 </div>
+                            <div> {data.address} </div>
                             <div className='text-brown pt-2'><HourglassTopIcon /> Few rooms left.</div>
                           </div>
                         </div>
@@ -144,10 +142,10 @@ export default function Searchresult() {
                       <Grid item sm={3}>
                         <div className='card'>
                           <div className='text-left'>
-                            <span> From ${data.rate} </span>
+                             From<span className='sub-header'> ${data.rate} </span>
                           </div>
                           <div className='text-left'>
-                            <Button variant="contained" color="success" size="small"> Select </Button>
+                            <Button variant="contained" color="success" size="small" onClick={(e) => { addToCart(e,data) }}> Add to Cart </Button>
                           </div>
                         </div>
                       </Grid>
@@ -156,13 +154,7 @@ export default function Searchresult() {
                 </Item>
 
               ))}
-              <div className='text-left'>
-                <Button variant="contained" color="success" size="small" onClick={sortAscending}> Ascending </Button>
-              </div>
-
-              <div className='text-left'>
-                <Button variant="contained" color="success" size="small" onClick={sortDecending}> Decending </Button>
-              </div>
+        
 
             </Grid>
           </Box>
