a4f5337b7ad8729a43d7dce3b711f7ef35d06edf
ashutosh-m <ashu3234@gmail.com>
2022-10-16 19:13:27 -0400

    added code for new signin page

--- storeflex/src/api/ApiConfig.ts
+++ storeflex/src/api/ApiConfig.ts
@@ -1,6 +1,13 @@
 
+export interface SignInProps {
+    username: string;
+    password: string;
+}
+
 export class ApiConfig {
     endPointGateWay = '/';
     testApi = '/test';
     signinApi = '/login';
+    signinTestApi = 'http://ec2-13-127-52-131.ap-south-1.compute.amazonaws.com:8082/storeflexservice/logintest';
+    signUpUrl = '///'
 }
