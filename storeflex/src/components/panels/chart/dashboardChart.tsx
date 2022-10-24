5eb67f1f01c09bb5ad22da5333e60d31d9d688b3
ashutosh-m <ashu3234@gmail.com>
2022-10-24 00:09:26 -0400

    added chart on dashboard

--- storeflex/src/components/panels/chart/dashboardChart.tsx
+++ storeflex/src/components/panels/chart/dashboardChart.tsx
@@ -1,16 +1,98 @@
 import { Chart } from "react-google-charts";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const DashboardChart = () => {
+
+    const bubbleChartData = [
+        ["ID", "Life Expectancy", "Fertility Rate", "Region", "Population"],
+        ["CAN", 80.66, 1.67, "North America", 33739900],
+        ["DEU", 79.84, 1.36, "Europe", 81902307],
+        ["DNK", 78.6, 1.84, "Europe", 5523095],
+        ["EGY", 72.73, 2.78, "Middle East", 79716203],
+        ["GBR", 80.05, 2, "Europe", 61801570],
+        ["IRN", 72.49, 1.7, "Middle East", 73137148],
+        ["IRQ", 68.09, 4.77, "Middle East", 31090763],
+        ["ISR", 81.55, 2.96, "Middle East", 7485600],
+        ["RUS", 68.6, 1.54, "Europe", 141850000],
+        ["USA", 78.09, 2.05, "North America", 307007000],
+    ];
+    const bubbleChartOptions = {
+        title:
+          "Correlation between life expectancy, fertility rate " +
+          "and population of some world countries (2010)",
+        hAxis: { title: "Life Expectancy" },
+        vAxis: { title: "Fertility Rate" },
+        bubble: { textStyle: { fontSize: 11 } },
+      };
+    const data = [
+        ["Year", "Sales", "Expenses", "Profit"],
+        ["2014", 1000, 400, 200],
+        ["2015", 1170, 460, 250],
+        ["2016", 660, 1120, 300],
+        ["2017", 1030, 540, 350],
+      ];
+    
+    const options = {
+        chart: {
+          title: "Company Performance",
+          subtitle: "Sales, Expenses, and Profit: 2014-2017",
+        },
+      };
+    
+    const pieChartData = [
+        ["Task", "Hours per Day"],
+        ["Work", 11],
+        ["Eat", 2],
+        ["Commute", 2],
+        ["Watch TV", 2],
+        ["Sleep", 7],
+      ];
+    const pieChartOptions = {
+        title: "My Daily Activities",
+        is3D: true,
+      };
     return (
-        <div> 
-            <Chart
-            chartType="ScatterChart"
-            data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-            width="100%"
-            height="400px"
-            legendToggle
-            />
-        </div>
+        <>
+        <Container className="p-bot-md">
+            <Row>
+                <Col sm={12} md={6}>
+                <Chart
+                    chartType="BubbleChart"
+                    width="100%"
+                    height="400px"
+                    data={bubbleChartData}
+                    options={bubbleChartOptions}
+                    />
+                </Col>
+                <Col sm={12} md={6}>
+                <Chart
+                    chartType="Bar"
+                    width="100%"
+                    height="400px"
+                    data={data}
+                    options={options}
+                />
+                </Col>
+            </Row>
+        </Container>
+        <Container className="p-bot-md">
+            <Row>
+                <Col sm={12} md={6}>
+                    <Chart
+                    chartType="PieChart"
+                    data={pieChartData}
+                    options={pieChartOptions}
+                    width={"100%"}
+                    height={"400px"}
+                    />
+                </Col>
+                <Col sm={12} md={6}>
+                </Col>
+            </Row>
+        </Container>
+        </>
     )
 } 
 export default DashboardChart
