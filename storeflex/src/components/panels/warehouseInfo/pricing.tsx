fe68ae02f799aef0c1f5e82688e4e1390c6409d8
Rupon Sarkar <rupon.skaplink@gmail.com>
2022-11-04 00:13:31 +0530

    Profile

---
+++ storeflex/src/components/panels/warehouseInfo/pricing.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Grid, TextareaAutosize, Button } from '@mui/material';
+import InputBox from '../../atoms/textfield/InputBox';
+import GetState from '../../atoms/state/GetState';
+
+const PricingFrom=()=>{
+    return(
+    <>
+     <div className='m-bot-lg'>
+                <div className='primary-gradient m-bot-md'>
+                    <div className='font-white p-sm f-18px f-bold'>Pricing</div>
+                </div>
+
+                <div className='p-md'>
+                    <div>
+                        <div>
+
+                            <Grid container spacing={2} columns={{ xs: 6, sm: 12, md: 12 }}>
+                            <Grid item xs={3}>
+                                    <InputBox data={{ name: 'facilityAddress', label: 'Total Available Space (sq.ft)*', value: '' }}
+                                    // onChange={validateAddress}
+                                    />
+                                    {/* {address && <p className="text-red">{address}</p>} */}
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <InputBox data={{ name: 'facilityAddress', label: 'Rate(Rs)/sq.ft/month *', value: '' }}
+                                    // onChange={validateAddress}
+                                    />
+                                    {/* {address && <p className="text-red">{address}</p>} */}
+                                </Grid>
+
+                                <Grid item xs={3}>
+                                    <InputBox data={{ name: 'zipcode', label: 'Minimum Order Quantity (sq.ft)*', value: '' }}
+                                    // onChange={validateZipCode}
+                                    />
+
+                                </Grid>
+
+
+                            </Grid>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+    </>
+    )
+}
+
+
+export default PricingFrom;
