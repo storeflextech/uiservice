0e7f3171633b4b073ef8f0303029da39086f9a71
Samaruzzaman <113083938+Samaruzzaman@users.noreply.github.com>
2022-11-04 08:37:59 +0530

    View Business

--- storeflex/src/components/business/view-business.tsx
+++ storeflex/src/components/business/view-business.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import swal from 'sweetalert';
@@ -13,24 +13,68 @@ import AddBusiness from './add-business';
 const ViewBusiness = () => {
     const navigate = useNavigate();
     const [modalShow, setModalShow] = React.useState(false);
+    const [businessInfo, setBusinessInfo] = useState<Array<any>>([]);
+
+    useEffect(() => {
+        const data = ([
+            {
+                id: 1,
+                CompanyName: 'Zaman & Sons Pvt. Ltd.',
+                CompanyURL: "https://zamanpvtltd.com",
+                State: "Assam",
+                City: "Guwahati",
+                Pincode: "781003",
+                Phone: "9988776655"
+            },
+            {
+                id: 2,
+                CompanyName: 'Rupon & Sons Pvt. Ltd.',
+                CompanyURL: "https://ruponpvtltd.com",
+                State: "Delhi",
+                City: "Central",
+                Pincode: "711003",
+                Phone: "9988776655"
+            },
+            {
+                id: 3,
+                CompanyName: 'Mrityunjoy & Sons Pvt. Ltd.',
+                CompanyURL: "https://mrityunjoypvtltd.com",
+                State: "Bihar",
+                City: "SomeCity",
+                Pincode: "741005",
+                Phone: "9988776655"
+            },
+            {
+                id: 4,
+                CompanyName: 'Brazen & Sons Pvt. Ltd.',
+                CompanyURL: "https://mrityunjoypvtltd.com",
+                State: "Meghalaya",
+                City: "Phulbari",
+                Pincode: "761002",
+                Phone: "9988776655"
+            },
+        ]);
+        setBusinessInfo(data);
+    }, [])
+
     const goToNextPage = (pagePath: string) => {
         navigate(pagePath);
     }
 
-    const deleteBusiness = (id) =>{
+    const deleteBusiness = (id) => {
         swal({
             title: "Are you sure?",
             text: "Do you really want to delete the business?",
             icon: "warning",
             dangerMode: true,
-          })
-          .then(willDelete => {
-            if (willDelete) {
-              swal("Deleted!", "Your business has been deleted!", "success");
-            }
-          });
+        })
+            .then(willDelete => {
+                if (willDelete) {
+                    swal("Deleted!", "Your business has been deleted!", "success");
+                }
+            });
     }
-    
+
     return (
         <AppContainer>
             <TopNavbar />
@@ -43,7 +87,7 @@ const ViewBusiness = () => {
                                 <div className='primary-gradient'>
                                     <div className='font-white p-sm f-18px f-bold'>
                                         Business
-                                        <button className="primary-btn-outline" style={{ fontSize: '14px', float: 'right', borderRadius: 20, paddingLeft: '12px', paddingRight: '12px' }} onClick={() => setModalShow(true)}><i className='mdi mdi-plus menu-icon'></i> Add New</button>
+                                        <button className="primary-btn-outline" style={{ fontSize: '14px', float: 'right', marginTop:'-10px', borderRadius: 20, right:'0px', padding:'7px 12px 7px 12px' }} onClick={() => setModalShow(true)}><strong><i className='mdi mdi-plus menu-icon'></i> Add New</strong></button>
                                     </div>
                                     <AddBusiness
                                         show={modalShow}
@@ -53,56 +97,32 @@ const ViewBusiness = () => {
                                 <Table striped bordered hover>
                                     <thead>
                                         <tr>
-                                            <th>#</th>
+                                            <th>Sl. No.</th>
                                             <th>Company Name</th>
                                             <th>Company URL</th>
                                             <th>State</th>
                                             <th>City</th>
                                             <th>Pincode</th>
                                             <th>Phone</th>
-                                            <th>Action</th>
+                                            <th style={{textAlign:'center'}}>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Zaman & Sons Company</td>
-                                            <td>https://zaman.com</td>
-                                            <td>Assam</td>
-                                            <td>Noida</td>
-                                            <td>211003</td>
-                                            <td>9998887766</td>
-                                            <td>
-                                                <button onClick={() => deleteBusiness(1)} className='primary-btn-outline' style={{ fontSize: '14px', float: 'right', borderRadius: 20, paddingLeft: '12px', paddingRight: '12px' }}><i className='mdi mdi-cup menu-icon'></i> Delete</button>
-                                                <button className='primary-btn-outline' style={{ fontSize: '14px', float: 'right', borderRadius: 20, paddingLeft: '12px', paddingRight: '12px' }}><i className='mdi mdi-pencil menu-icon'></i> Edit</button> &nbsp; &nbsp;
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Rupon & Sons</td>
-                                            <td>https://rupon.com</td>
-                                            <td>Delhi</td>
-                                            <td>Guwahati</td>
-                                            <td>781003</td>
-                                            <td>78956498765</td>
-                                            <td>
-                                                <button onClick={() => deleteBusiness(2)} className='primary-btn-outline' style={{ fontSize: '14px', float: 'right', borderRadius: 20, paddingLeft: '12px', paddingRight: '12px' }}><i className='mdi mdi-cup menu-icon'></i> Delete</button>
-                                                <button className='primary-btn-outline' style={{ fontSize: '14px', float: 'right', borderRadius: 20, paddingLeft: '12px', paddingRight: '12px' }}><i className='mdi mdi-pencil menu-icon'></i> Edit</button> &nbsp; &nbsp;
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>Mrityunjoy & Sons</td>
-                                            <td>https://mrityunjoy.com</td>
-                                            <td>Kolkata</td>
-                                            <td>Kolkata</td>
-                                            <td>881005</td>
-                                            <td>68956498765</td>
-                                            <td>
-                                                <button  onClick={() => deleteBusiness(3)} className='primary-btn-outline'  style={{ fontSize: '14px', float: 'right', borderRadius: 20, paddingLeft: '12px', paddingRight: '12px' }}><i className='mdi mdi-cup menu-icon'></i> Delete</button>
-                                                <button className='primary-btn-outline' style={{ fontSize: '14px', float: 'right', borderRadius: 20, paddingLeft: '12px', paddingRight: '12px' }}><i className='mdi mdi-pencil menu-icon'></i> Edit</button> &nbsp; &nbsp;
-                                            </td>
-                                        </tr>
+                                        {businessInfo.map((item) => (
+                                            <tr>
+                                                <td>{item.id}</td>
+                                                <td>{item.CompanyName}</td>
+                                                <td>{item.CompanyURL}</td>
+                                                <td>{item.State}</td>
+                                                <td>{item.City}</td>
+                                                <td>{item.Pincode}</td>
+                                                <td>{item.Phone}</td>
+                                                <td>
+                                                    <button onClick={() => deleteBusiness(item.id)} className='primary-btn-outline' style={{ fontSize: '14px', float: 'right', borderRadius: 20, padding:'8px 12px 8px 12px' }}><strong><i className='mdi mdi-cup menu-icon'></i> Delete</strong></button>&nbsp; &nbsp;
+                                                    <button className='primary-btn-outline' style={{ fontSize: '14px', float: 'right', borderRadius: 20, padding:'8px 12px 8px 12px',marginRight:'5px' }}><strong><i className='mdi mdi-pencil menu-icon'></i> Edit</strong></button>
+                                                </td>
+                                            </tr>
+                                        ))}
                                     </tbody>
                                 </Table>
                             </div>
