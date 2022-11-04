131069e99ddb7ca2e7ce24cc672e9013571a3f03
mrityunjoy456 <mrityunjoydeka456@gmail.com>
2022-11-04 17:00:49 +0530

    little change

--- storeflex/src/components/miscellaneous/services.tsx
+++ storeflex/src/components/miscellaneous/services.tsx
@@ -1,6 +1,40 @@
 import React from 'react';
 
 const Services = () => {
+
+  const handleCard=(data)=>{
+    console.log(data);
+
+  switch(data){
+    case 'one':
+      console.log("Clicked on 1st card");
+      return;
+
+    case 'two':
+      console.log("clicked on 2nd card");
+      return;
+
+    case 'three':
+      console.log("clicked on 3rd card");
+      return;
+
+    case 'four':
+      console.log("clicked on 4th card");
+      return;
+
+    default: console.log("erro");
+    return;
+    
+      
+      
+      
+  }
+    
+  
+
+  }
+
+
     return (
       <>
       <section id="services" className="services-area services-seven">
@@ -23,53 +57,51 @@ const Services = () => {
         <div className="container">
           <div className="single-head">
             <div className="row">
-              <div className="col-lg-3 col-md-4 col-12">
+              <div className=" col-lg-3 col-md-4 col-12">
                 <div className="single-service">
                   <span className="serial">01</span>
                   <h4>Listing Platform</h4>
                   <p>
-                    Empowering Consumers with data,inspiration & knowlwdge for leasing.
+                    Empowering Consumers with data,inspiration & knowlwdge...
                   </p>
                   <div className="light-rounded-buttons">
-                    <a href="javascript:void(0)" className="btn primary-btn"> Read More </a>
+                    <button onClick={()=>handleCard("one")} className="btn primary-btn btn-sm"> Read More </button>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-3 col-md-6 col-12">
+              <div className="col-lg-3 col-md-4 col-12">
                 <div className="single-service">
                   <span className="serial">02</span>
                   <h4>Resource Management</h4>
                   <p>
-                    We dejoy working with discerning clients, people for whom
-                    qualuty, service, integrity & aesthetics.
+                    We dejoy working with discerning clients, people...
                   </p>
                   <div className="light-rounded-buttons">
-                    <a href="javascript:void(0)" className="btn primary-btn"> Read More </a>
+                  <button onClick={()=>handleCard("two")} className="btn primary-btn btn-sm"> Read More </button>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-3 col-md-6 col-12">
+              <div className="col-lg-3 col-md-4 col-12">
                 <div className="single-service">
                   <span className="serial">03</span>
                   <h4>Technology backed warehousing solution</h4>
                   <p>
-                    Browse rentals with up-to-date information,tools
-                    and resources to help you stress less,detailed description,floorplans and more.
+                    Browse rentals with clients, people...
                   </p>
                   <div className="light-rounded-buttons">
-                    <a href="javascript:void(0)" className="btn primary-btn"> Read More </a>
+                  <button onClick={()=>handleCard("three")} className="btn primary-btn btn-sm"> Read More </button>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-3 col-md-6 col-12">
+              <div className="col-lg-3 col-md-4 col-12" >
                 <div className="single-service">
                   <span className="serial">04</span>
                   <h4>Accounting & Transaction Services</h4>
                   <p>
-                    We take care of the lease and payment processing.
+                    We take care of the lease and...
                   </p>
                   <div className="light-rounded-buttons">
-                    <a href="javascript:void(0)" className="btn primary-btn"> Read More </a>
+                  <button onClick={()=>handleCard("four")} className="btn primary-btn btn-sm"> Read More </button>
                   </div>
                 </div>
               </div>
