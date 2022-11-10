322e1e29f963c72b4bb055b83d5d70ea193cc7a4
ashutosh-m <ashu3234@gmail.com>
2022-11-09 20:03:04 -0500

    added content componemnt

---
+++ storeflex/src/components/atoms/contact/contacts.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Contacts = () => {
+    return (
+        <div className='sf-contact-container'>
+            <div className='sf-contact-holder '>
+                <div className='sf-contact-img'>
+                    <img src="/images/country/india.jpeg" alt="contact" />
+                </div>
+                <div className='sf-contact-text'>
+                    <h5><span>India</span></h5>
+                    <p><span> Tel : 012346568  </span></p>
+                    <p><span> Addres : Sector 10 , Noida   </span></p>
+                </div>
+            </div>
+        </div>
+    )
+} 
+
