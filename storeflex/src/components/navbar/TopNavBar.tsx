c031f75147b4131ed637b81f6953c5496a1044d5
ashutosh-m <ashu3234@gmail.com>
2022-11-23 00:58:14 -0500

    added href for buttons

--- storeflex/src/components/navbar/TopNavBar.tsx
+++ storeflex/src/components/navbar/TopNavBar.tsx
@@ -37,7 +37,7 @@ const TopNavBar = () => {
     <Navbar fixed="top" collapseOnSelect expand="md" className="sf-bg-color-primary" variant="dark">
       <div className='sf-flex sf-justify w100'>
       <Container className='top-nav-container'>
-        <Navbar.Brand href="home">
+        <Navbar.Brand href="/home">
           <span className='top-nav-logo'>
           <img src="../../assets/images/white-logo.jpg" alt="Logo" />
           </span>
@@ -45,11 +45,11 @@ const TopNavBar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className='top-nav-link' href="#hero-area">Home</Nav.Link>
-              <Nav.Link className='top-nav-link' href="#services">Services</Nav.Link>
-              <Nav.Link className='top-nav-link' href="#pricing">Featured WH</Nav.Link>
-              <Nav.Link className='top-nav-link' href="#clients">Clients</Nav.Link>
-              <Nav.Link className='top-nav-link' href="#contact">Contact</Nav.Link>
+              <Nav.Link className='top-nav-link' href="/home#hero-area">Home</Nav.Link>
+              <Nav.Link className='top-nav-link' href="/home#services">Services</Nav.Link>
+              <Nav.Link className='top-nav-link' href="/home#pricing">Featured WH</Nav.Link>
+              <Nav.Link className='top-nav-link' href="/home#clients">Clients</Nav.Link>
+              <Nav.Link className='top-nav-link' href="/home#contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
           
