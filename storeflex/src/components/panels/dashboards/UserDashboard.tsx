990db77924c55c47cee1f0800081b330e25122fc
spidernishanta <nishantann@gmail.com>
2022-11-28 13:12:51 +0530

    Changes in navigation

--- storeflex/src/components/panels/dashboards/UserDashboard.tsx
+++ storeflex/src/components/panels/dashboards/UserDashboard.tsx
@@ -25,7 +25,7 @@ const UserDashboard = (props) => {
             </div>
             <div>
             <div className='sf-flex-grid sf-justify'>
-                    <Button className='m-sm sf-btn w100' size="lg" active onClick={() => { handelOnClick(PAGES.Business.path) }}>
+                    <Button className='m-sm sf-btn w100' size="lg" active onClick={() => { handelOnClick(PAGES.Business.ADD.path) }}>
                         {dashboardContent?.addBusinessBtn}
                     </Button>
 
