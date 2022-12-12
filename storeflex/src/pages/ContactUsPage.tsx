20ad1d2b70962c2e4300b384c2f9a361e24a3a24
s0Fi98 <solmansofihussain@gmail.com>
2022-12-12 11:35:30 +0530

    little changes

--- storeflex/src/pages/ContactUsPage.tsx
+++ storeflex/src/pages/ContactUsPage.tsx
@@ -5,6 +5,7 @@ import ContactUs from '../components/panels/contactus/ContactUs';
 const ContactUsPage = () => {
     return(
       <>
+      {/* This is for a little change */}
         <section id="contact" className="contact-us">
           {<ContactUs />}
         </section>
