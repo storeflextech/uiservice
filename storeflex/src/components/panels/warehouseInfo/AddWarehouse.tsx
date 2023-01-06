9a0376268b560b69bc1b04b66da1b257ed950192
Jintu-png <60797876+Jintu-png@users.noreply.github.com>
2023-01-06 16:06:54 +0530

    Please verify the code and merge

--- storeflex/src/components/panels/warehouseInfo/AddWarehouse.tsx
+++ storeflex/src/components/panels/warehouseInfo/AddWarehouse.tsx
@@ -10,7 +10,10 @@ import WarehouseLayout, {WarehouseLayoutObj} from './component/WarehouseLayout';
 import WarehouseDetails from './component/WarehouseDetails';
 import { WarehousePostData, Hours } from '../../../api/ApiConfig'; 
 import { WhDetail } from './component/WarehouseDetails';
-import { Address, Warehouseprice } from '../../../utils/ResponseSchema';
+import { Address, objectData, Warehouseprice } from '../../../utils/ResponseSchema';
+import { Value } from 'sass';
+import { AddBusiness } from '@mui/icons-material';
+
 
 const AddWarehouse = () => {
 
@@ -21,6 +24,8 @@ const AddWarehouse = () => {
     const [pricing, setPricing] = useState<Warehouseprice>({});
     const [whHours, setWhHours] = useState<Hours>({});
     const [whLayout, setLayout] = useState<WarehouseLayoutObj>({});
+    
+    const[gstid, setGstId]= useState<WhDetail>({});
 
     const onWarehouseDetailsUpdate = (data: WhDetail) => {
         setWhDetails(data);
@@ -44,7 +49,55 @@ const AddWarehouse = () => {
     }
 
     const addWarehouse = () => {
-        const buildPostData = {} as WarehousePostData;
+        
+        if(!whDetails?.warehouseName){
+            alert('Warehouse Name is Required');
+        }
+        else if(!whDetails?.descp){
+            alert('Warehouse Description is required');
+        }
+        else if(!whDetails?.warehouseTaxId){
+            alert('gst is required');
+        }
+        else if(!whDetails?.clientId){
+            alert('Client id is required');
+        }
+        else if(!whAddress?.plotNo){
+            alert('Plot No is required');
+        }
+        else if(!whAddress?.houseNo){
+            alert('House No is required');
+        }
+        else if(!whAddress?.streetDetails){
+            alert('Street No is required');
+        }
+        else if(!whAddress?.state){
+            alert('State is required');
+        }
+        else if(!whAddress?.city){
+            alert('City is required');
+        }
+        else if(!whAddress?.country){
+            alert('country is required');
+        }
+        else if(!whAddress?.pincode){
+            alert('Pincode is required');
+        }
+        else if(! whLayout?.dockhighdoors){
+            alert('Dock high door is required');
+        }
+        else if(!whLayout?.atgradedoors){
+            alert('At grade door is required');
+        }
+        else if(!whLayout?.ceillingheight){
+            alert('Ceilling height is required');
+        }
+        else if(!whLayout?.forkliftcapacity){
+            alert('Fork lift capacity is required');
+        }
+      
+         else{
+            const buildPostData = {} as WarehousePostData;
         buildPostData.clientId = whDetails?.clientId;
         buildPostData.warehouseName = whDetails?.warehouseName;
         buildPostData.warehouseTaxId = whDetails?.warehouseTaxId;
@@ -58,7 +111,8 @@ const AddWarehouse = () => {
         buildPostData.atgradedoors = whLayout.atgradedoors;
         buildPostData.ceillingheight = whLayout.ceillingheight;
         buildPostData.forkliftcapacity = whLayout.forkliftcapacity;
-        buildPostData.warehouseprice = pricing;
+       
+        
 
         setIsLoader(true);
         api.addWarehouse(buildPostData).then((resp) => {
@@ -73,6 +127,7 @@ const AddWarehouse = () => {
             setIsLoader(false);
             console.log(' addWarehouse creation erroor ', error);
         });
+        }
     }
     return (
         <>
